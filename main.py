3dc12453943b434070459126c9943713eeccb7ce
Luis Cuevas <lulacuna16@gmail.com>
2020-02-16 13:33:16 -0600

    Version2 (Imprime corretamente tablero)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print ("Toca realizar el ejercicio ",dias%3,": ", practica(dias%3))  """
 
 # REALIZAR GATO DUMMY
 #CREANDO MATRIZ
-def matrizP():
+def matrizP(): #Tablero Principiante
     filas=["1","2","3"];
     columnas=["A","B","C"];
     alto = len(filas) + 1
@@ -23,13 +23,18 @@ def matrizP():
         matriz.append([])
         for j in range(largo):  # LARGO
             matriz[i].append(" ")
-    """alto = n
-    largo = n
-    matriz = []
+    return generarMatrizInicial(matriz,filas,columnas)
+
+def matrizA(): #Tablero avanzado
+    filas=["1","2","3","4","5"];
+    columnas=["A","B","C","D","E"];
+    alto = len(filas) + 1
+    largo=len(columnas)+1;
+    matriz=[]
     for i in range(alto):  # ALTO
         matriz.append([])
         for j in range(largo):  # LARGO
-            matriz[i].append(" ")"""
+            matriz[i].append(" ")
     return generarMatrizInicial(matriz,filas,columnas)
 
 #GENERAR LINEAS DEL PRIMER CUADRANTE
@@ -49,21 +54,14 @@ def generarMatrizInicial(matriz,filas,columnas):
                 else:
                     matriz[i][j]="-"
     return matriz
-    """"" alto=len(matriz)
-    largo=len(matriz[0])
-    for i in range(alto):  # ALTO
-        for j in range(largo):  # LARGO
-            if j is 0:
-                matriz[i][j] = chr(65 + i)
-            else:
-                for j in range(largo):  # LARGO
-                    matriz[i][j] = "-"" """
 
 
 def verMatriz(matriz):
-    for i in range(len(matriz)):
-        for j in range(len(matriz[0])):  # LARGO
-            print(matriz[i][j],"\n")
-
-matriz=matrizP()
-verMatriz(matriz)
+    alto = len(matriz)
+    largo = len(matriz[0])
+    for i in range(alto):
+        for j in range(largo):  # LARGO
+            print(matriz[i][j], "\t", end=" ")
+        print()
+matriz1=matrizA()
+verMatriz(matriz1)
